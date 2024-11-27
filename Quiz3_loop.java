9feebe65aba505f364793068c756cb8351cfb29f
jzeili <caminadelezieljean@gmail.com>
2024-11-28 01:13:36 +0800

    Add files via upload

---
+++ Quiz3_loop.java
@@ -0,0 +1,12 @@
+public class Quiz3_loop {
+    public static void main(String[] args) {
+        
+       int x = 10;
+        while (x <= 100) {
+           
+            System.out.println(x);
+             x += 10;
+        }
+    }
+    
+}
